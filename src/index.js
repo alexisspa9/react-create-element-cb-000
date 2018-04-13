e142c63c07d38eec234aa0ef1c4d3586829b351a
Alexander Spanos <alexisspa9@gmail.com>
2018-04-13 09:16:16 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 // Code goes here
-import React from 'react'
-import ReactDOM from 'react-dom'
-const title = React.createElement('h1', {}, 'My first React code!');
-const paragraph = React.createElement('p', {}, 'Writing some more HTML. Cool stuff!');
-const container = React.createElement('div', {}, [title, paragraph]);
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const list =
+  React.createElement('div', {},
+    React.createElement('h1', {}, 'My favorite ice cream flavors'),
+    React.createElement('ul', {},
+      [
+        React.createElement('li', { class: 'brown' }, 'Chocolate'),
+        React.createElement('li', { class: 'white' }, 'Vanilla'),
+        React.createElement('li', { class: 'yellow' }, 'Banana')
+      ]
+    )
+  );
+
 ReactDOM.render(
-  container,
+  list,
   document.getElementById('global')
 );
