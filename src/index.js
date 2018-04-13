8c4d7e36205a952a30d3a2bd7e87fe21cd7c5ab2
Alexander Spanos <alexisspa9@gmail.com>
2018-04-13 09:12:05 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 // Code goes here
 import React form 'react'
-import ReactDom from 'react-dom'
+import ReactDOM from 'react-dom'
 const title = React.createElement('h1', {}, 'My first React code!');
+
+ReactDOM.render(
+  title,
+  document.getElementById(global)
+);
