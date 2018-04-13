dde5a98e41d6b3fb2a995ec8780288a1214dadc3
Alexander Spanos <alexisspa9@gmail.com>
2018-04-13 09:14:42 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 const title = React.createElement('h1', {}, 'My first React code!');
-
+const container = React.createElement('div', {}, title);
 ReactDOM.render(
   title,
   document.getElementById('global')
